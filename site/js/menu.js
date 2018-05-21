4e3baafeb34706c1af6da36b0c2c7ecdbc288178
Jakob Ristner <jakob.ristner@itggot.se>
2018-05-21 16:25:36 +0200

    progress on hamburger menu

---
+++ site/js/menu.js
@@ -0,0 +1,8 @@
+function togglemenu() {
+    var element = document.querySelector(".close");
+    element.classList.toggle("show");
+
+    var element = document.querySelector(".fade-layer");
+    element.classList.toggle("show");
+
+}
